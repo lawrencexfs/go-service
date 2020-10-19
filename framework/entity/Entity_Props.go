2a027e2b103316b39372af43c017af1ab765f94a
yekoufeng <357262356@qq.com>
2020-10-19 12:29:34 +0800

    Entity_Props.go 拆成 Entity_Props.go Entity_db.go

--- framework/entity/Entity_Props.go
+++ framework/entity/Entity_Props.go
@@ -4,22 +4,14 @@ import (
 	"fmt"
 	"reflect"
 
-	dbservice "github.com/giant-tech/go-service/base/mongodbservice"
 	"github.com/giant-tech/go-service/base/stream"
 
 	log "github.com/cihub/seelog"
-	"github.com/globalsign/mgo/bson"
 
 	"github.com/giant-tech/go-service/framework/idata"
 
 	"github.com/spf13/viper"
 )
-
-// PropDBName PropDBName
-var PropDBName string
-
-// PropTableName PropTableName
-var PropTableName string
 
 // InitProp 初始化属性列表
 func (e *Entity) InitProp(def *Def, loadFromDB bool) {
@@ -217,90 +209,6 @@ func (e *Entity) UpdateFromMsg(num int, data []byte) {
 			return
 		}
 	}
-}
-
-// loadFromDB 从数据库中恢复
-func (e *Entity) loadFromDB() {
-	if e.GetEntityID() == 0 {
-		return
-	}
-
-	selectProps := bson.M{}
-	for k, v := range e.props {
-		if v.def.Persistence {
-			selectProps[k] = 1
-		}
-	}
-
-	retRawData := bson.Raw{}
-	retM := bson.M{}
-	var tempDBElems []bson.RawDocElem
-
-	log.Debug("loadFromDB MongoDBQueryOneWithSelect: ", " , PropDBName: ", PropDBName, " , PropTableName: ", PropTableName, " , selectProps", selectProps)
-
-	dbservice.MongoDBQueryOneWithSelect(PropDBName, PropTableName, bson.M{"dbid": e.GetEntityID()}, selectProps, &retRawData)
-
-	bson.Unmarshal(retRawData.Data, &retM)
-	bson.Unmarshal(retRawData.Data, &tempDBElems)
-
-	//log.Info("select props: ", selectProps, "props return: ", ret)
-
-	for k, v := range retM {
-		if k == "_id" {
-			continue
-		}
-
-		info, ok := e.props[k]
-		if ok {
-			log.Debug("loadFromDB, key is : ", k)
-			info.UnPackMongoValue(v, tempDBElems)
-		} else {
-			log.Error("loadFromDB, prop not exist: ", k)
-		}
-	}
-
-	/*for _, elem := range tempDBElems {
-
-		if elem.Name == "_id" {
-			continue
-		}
-
-		info, ok := e.props[elem.Name]
-		if ok {
-			info.UnPackMongoValue(elem.Value.Data)
-
-		} else {
-
-		}
-	}
-
-	*/
-}
-
-// SavePropsToDB 属性保存到db
-func (e *Entity) SavePropsToDB() {
-	if e.GetEntityID() == 0 {
-		return
-	}
-
-	if len(e.dirtySaveProps) == 0 {
-		return
-	}
-
-	log.Debug("SyncProps len: ", len(e.dirtySaveProps))
-
-	saveMap := bson.M{}
-	for n, p := range e.dirtySaveProps {
-		saveMap[n] = p.GetValue()
-
-		p.dbFlag = false
-	}
-
-	log.Info("prop saveMap: ", saveMap)
-
-	dbservice.MongoDBUpdate(PropDBName, PropTableName, bson.M{"dbid": e.GetEntityID()}, bson.M{"$set": saveMap})
-
-	e.dirtySaveProps = make(map[string]*PropInfo)
 }
 
 // PackProps 打包属性
